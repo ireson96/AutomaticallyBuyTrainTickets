7876720cde5568c8fca6680d125e3b617b6bec77
ireson96 <769255844@qq.com>
2018-04-08 22:56:27 +0800

    add model

---
+++ src/semiAutomatic/tempConfigModle.java
@@ -0,0 +1,9 @@
+package semiAutomatic;
+
+public class tempConfigModle {
+	static public String username;
+	static public String password;
+	static public String fromStation;
+	static public String toStation;
+	static public String[] passengers;
+}
